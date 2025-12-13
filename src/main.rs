049c766929143bf22514c87a17d780f4376bde03
LJ Li <liliangjya@gmail.com>
2025-12-14 00:39:40 +0800

    feat: add test

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
 // src/main.rs
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        // Keep panic output on one line (nightly's default format splits across lines).
+        println!(
+            "panicked at '{}', {}:{}:{}",
+            info.message(),
+            location.file(),
+            location.line(),
+            location.column()
+        );
+    } else {
+        println!("panicked: {}", info.message());
+    }
     loop {}
 }
 
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
 #[unsafe(no_mangle)] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
